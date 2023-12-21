6fc681cc19576588bee4b639a7428306c8eab9b4
master <736377594@qq.com>
2023-12-21 23:22:05 +0800

    reset base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/github-pages-demo-1/dist/',
+  // base: '/github-pages-demo-1/dist/',
   plugins: [vue()],
 
 })
